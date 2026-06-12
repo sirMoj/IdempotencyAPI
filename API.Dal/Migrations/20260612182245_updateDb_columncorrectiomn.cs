6e883065602870a061579d5e5553cc36224be2b4
sirMoj <serranojoshua790@gmail.com>
2026-06-13 02:27:12 +0800

    Commit bugfix

---
+++ API.Dal/Migrations/20260612182245_updateDb_columncorrectiomn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateDb_columncorrectiomn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "requesHash",
+                table: "idempotencyRecords",
+                newName: "requestHash");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "requestHash",
+                table: "idempotencyRecords",
+                newName: "requesHash");
+        }
+    }
+}
